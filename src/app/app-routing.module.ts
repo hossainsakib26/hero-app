69e5de5abd004fed95663457437e61acec9d49f4
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-05-21 22:45:09 +0600

    add router outlet in appcomponent html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {HeroesComponent} from "./hero-app/heroes/heroes.component";
 
 
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent}
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes) // using forRoot() because configuring the router at the application root level.
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
