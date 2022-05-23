5d325c1970c980621ea702e02f8fa80dc8a1ca90
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-05-23 11:54:14 +0600

    add specific id base search navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HeroesComponent} from "./hero-app/heroes/heroes.component";
 import {DashboardComponent} from "./hero-app/dashboard/dashboard.component";
+import {HeroDetailComponent} from "./hero-app/hero-detail/hero-detail.component";
 
 const routes: Routes = [
   {path: 'heroes', component: HeroesComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'detail/:id', component: HeroDetailComponent},
 ];
 
 @NgModule({
