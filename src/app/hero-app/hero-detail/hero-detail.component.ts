8d913b8f8561944edcf930e303214305c33bac4d
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-05-23 22:50:53 +0600

    activatedrout added to hero details component

--- src/app/hero-app/hero-detail/hero-detail.component.ts
+++ src/app/hero-app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 
-import { Hero } from '../modal/hero';
+import {Hero} from '../modal/hero';
+import {HeroService} from '../services/hero.service';
 
 @Component({
   selector: 'hero-detail',
@@ -12,9 +15,11 @@ export class HeroDetailComponent implements OnInit {
   // property
   @Input() hero?: Hero
 
-  constructor() { }
+  constructor(private rout: ActivatedRoute, private location: Location, private heroService: HeroService) {}
 
   ngOnInit(): void {
+    const id = Number(this.rout.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
 
 }
