cccf554ac60129187fec9a6aec149f8521ccc82d
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-05-24 20:21:36 +0600

    httpclient added in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http'
 
 import {HeroesComponent} from './hero-app/heroes/heroes.component';
 import {HeroDetailComponent} from './hero-app/hero-detail/hero-detail.component';
 import {MessagesComponent} from './common-needs/messages/messages.component';
-import { DashboardComponent } from './hero-app/dashboard/dashboard.component';
+import {DashboardComponent} from './hero-app/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { DashboardComponent } from './hero-app/dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
