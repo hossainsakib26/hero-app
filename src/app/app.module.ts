cc5fc6eb048cae45c4c70d7ae381ec7c3f03d1c6
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-05-25 10:54:59 +0600

    install angular-in-memory-web-api and in-memory-data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http'
+// learning how angular interact with server
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-web-api/in-memory-data.service';
 
 import {HeroesComponent} from './hero-app/heroes/heroes.component';
 import {HeroDetailComponent} from './hero-app/hero-detail/hero-detail.component';
@@ -23,6 +26,7 @@ import {DashboardComponent} from './hero-app/dashboard/dashboard.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
