b5805e9365eeef80f5ec9c05a0237e554bbcfee1
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-06-16 12:55:57 +0600

    data search work but search with letter not work

--- src/app/hero-app/services/hero.service.ts
+++ src/app/hero-app/services/hero.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 //import observable for data, [of is a rxjs function symbol
-import {Observable, of} from  'rxjs';
+import {Observable, of} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {catchError, map, tap} from 'rxjs/operators';
 
@@ -16,35 +16,39 @@ export class HeroService {
   private heroesUrl: string = 'http://localhost:3000'; //url to web api
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
   };
 
-  constructor(private messageService: MessageService, private http: HttpClient) { }
+  constructor(private messageService: MessageService, private http: HttpClient) {
+  }
 
   getHeroes(): Observable<Hero[]> {
     const url = this.heroesUrl + '/heroes';
-    return this.http.get<Hero[]>(url).pipe(
+    const datas = this.http.get<Hero[]>(url).pipe(
       tap(_heroes => this.log(`fatched/got heroes`)),
       catchError(this.handleError<Hero[]>('getHeroes', []))
     );
+    return datas;
   }
 
   /* GET heroes by name contains on search */
-  searchHeroes(term: string): Observable<Hero[]>{
-    if (!term.trim()){return of([])}
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([])
+    }
     const url = this.heroesUrl + `/heroes?name=${term}`;
-
-    return this.http.get<Hero[]>(`${url}`).pipe(
+    const datam = this.http.get<Hero[]>(`${url}`).pipe(
       tap(
         x => x.length ?
           this.log(`found heroes matching "${term}"`) : this.log(`no heroes matching "${term}"`)),
       catchError(this.handleError<Hero[]>(`SearchHeroes`, []))
     );
+    return datam;
   }
 
   /** GET hero by id. Will 404 if id not found */
-  getHero(id: number): Observable<Hero>{
-    const hero = this.http.get<Hero>(this.heroesUrl+`/heroes/${id}`).pipe(
+  getHero(id: number): Observable<Hero> {
+    const hero = this.http.get<Hero>(this.heroesUrl + `/heroes/${id}`).pipe(
       tap(_hero => this.log(`fatch/get hero id = ${id}`)),
       catchError(this.handleError<Hero>('getHero'))
     );
@@ -52,17 +56,17 @@ export class HeroService {
   }
 
   /*PUT: update hero on the server and using put method */
-  updateHero(hero: Hero): Observable<any>{
+  updateHero(hero: Hero): Observable<any> {
     const serverUrl = this.heroesUrl + '/heroes/'
     // put has 3 parameters url, object and options
-    return this.http.put((serverUrl+`${hero.id}`), hero, this.httpOptions).pipe(
+    return this.http.put((serverUrl + `${hero.id}`), hero, this.httpOptions).pipe(
       tap(_hero => this.log(`Updated hero id = ${hero.id} Updated Name: ${hero.name}`)),
       catchError(this.handleError<any>('updateHero'))
     );
   }
 
   /** POST: add a new hero to the server */
-  addHero(hero: Hero): Observable<Hero>{
+  addHero(hero: Hero): Observable<Hero> {
     const serverUrl = this.heroesUrl + '/heroes'
     return this.http.post<Hero>(serverUrl, hero, this.httpOptions).pipe(
       tap((newHero: Hero) => this.log(`added hero w/ id = ${newHero.id}`)),
@@ -71,7 +75,7 @@ export class HeroService {
   }
 
   /** DELETE: delete the hero from the server */
-  deleteHero(id: number): Observable<Hero>{
+  deleteHero(id: number): Observable<Hero> {
     const url = this.heroesUrl + `/heroes/${id}`;
     return this.http.delete<Hero>(url, this.httpOptions).pipe(
       tap(selectedHero => this.log(`deleted hero ${selectedHero.id}`)),
@@ -90,7 +94,7 @@ export class HeroService {
    * @param operation - name of the operation that failed
    * @param result - optional value to return as the observable result
    */
-  private handleError<T>(operation = 'operation', result?: T){
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
       // TODO: send the error to remote logging infrastructure
